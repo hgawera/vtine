6caa54c7c3b4a5ccebb901ca30d946cc8ec47462
Harneet <harneetgawera@gmail.com>
2026-02-02 21:41:28 +0000

    Finished valentine site with custom message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [noButtonPos, setNoButtonPos] = useState({ top: '55%', left: '60%' });
+  const [isAccepted, setIsAccepted] = useState(false);
+
+  const moveButton = () => {
+    const randomTop = Math.floor(Math.random() * 60 + 20) + '%';
+    const randomLeft = Math.floor(Math.random() * 60 + 20) + '%';
+    setNoButtonPos({ top: randomTop, left: randomLeft });
+  };
+
+  if (isAccepted) {
+    return (
+      <div className="App">
+        <h1>Yay! I love you! ❤️</h1>
+        <p className="love-message">
+          I love you so much my love. To many more valentines together. 
+          I hope I make you happy. I know life is tough right now but 
+          you are so much stronger than you know and I am so proud of you. 
+          You make me the happiest person in the world. I LOVE YOU FOREVER.
+        </p>
+        <img src="/yes.gif" alt="Celebration" className="gif" />
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Will you be my Valentine? 💌💘💝</h1>
+
+      <div className="button-group">
+        <button className="btn-yes" onClick={() => setIsAccepted(true)}>
+          Yes
+        </button>
+
+        <button 
+          className="btn-no"
+          onMouseEnter={moveButton}
+          style={{ 
+            position: 'absolute', 
+            top: noButtonPos.top, 
+            left: noButtonPos.left 
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          No
+        </button>
+      </div>
+
+      <img src="/happy.gif" alt="Waiting" className="gif" />
     </div>
   );
 }
